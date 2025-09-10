c6df368134dd16331664497c40c059fc62216efd
iara.cgr <gcamila.jobs@gmail.com>
2025-09-09 21:46:03 -0300

    area rectangulo

--- src/tp1/AreaRectangulo.java
+++ src/tp1/AreaRectangulo.java
@@ -1,4 +1,16 @@
 package tp1;
 
+import java.util.Scanner;
+
 public class AreaRectangulo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese la base del rectángulo:");
+        int base = s.nextInt();
+        System.out.println("Ingrese la altura del rectángulo:");
+        int altura = s.nextInt();
+        int area = base * altura;
+        System.out.println("El área del rectángulo es: "+ area);
+
+    }
 }
